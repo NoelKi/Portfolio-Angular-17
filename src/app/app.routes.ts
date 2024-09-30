f973c3f8e9830b69eaee0b7709beee45686d2d66
Kieran Mai <kieran.noel@icloud.com>
2024-09-30 10:09:47 +0200

    add legal notice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 
 export const routes: Routes = [
   { path: '', component: AboveTheFoldComponent },
   { path: 'Imprint', component: ImprintComponent },
+  { path: 'LegalNotice', component: LegalNoticeComponent },
 ];
