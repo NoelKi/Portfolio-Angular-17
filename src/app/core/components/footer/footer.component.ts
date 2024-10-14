5a4a525bef0184c5cebcd8ea51304f2a1b270a97
Kieran Mai <kieran.noel@icloud.com>
2024-10-14 12:42:48 +0200

    add bottom links

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -8,4 +8,32 @@ import { RouterLink } from '@angular/router';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  mailSource = 'assets/img/icons/footer-mail.svg';
+  linkedInSource = 'assets/img/icons/footer-linkedin.svg';
+  githubSource = 'assets/img/icons/footer-github.svg';
+
+  mailHover(): void {
+    this.mailSource = 'assets/img/icons/footer-mail-hover.svg';
+  }
+
+  mailDefault(): void {
+    this.mailSource = 'assets/img/icons/footer-mail.svg';
+  }
+
+  linkedHover(): void {
+    this.linkedInSource = 'assets/img/icons/footer-linkedin-hover.svg';
+  }
+
+  linkedDefault(): void {
+    this.linkedInSource = 'assets/img/icons/footer-linkedin.svg';
+  }
+
+  githubHover(): void {
+    this.githubSource = 'assets/img/icons/footer-github-hover.svg';
+  }
+
+  githubDefault(): void {
+    this.githubSource = 'assets/img/icons/footer-github.svg';
+  }
+}
